e139641e5e0e8c12d7e7c8fc8d07771a207e0582
gopalvashishtha <gopalvashishtha2000@gmail.com>
2020-09-18 23:40:18 +0530

    Added new XML file

--- app/src/main/java/com/example/chitraseakshar/MainActivity.kt
+++ app/src/main/java/com/example/chitraseakshar/MainActivity.kt
@@ -1,11 +1,68 @@
 package com.example.chitraseakshar
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Message
+import android.provider.MediaStore
+import android.util.Log
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import com.github.dhaval2404.imagepicker.ImagePicker
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
+import com.google.firebase.ml.vision.text.FirebaseVisionText
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
+    companion object{
+        private val TAG = MainActivity::class.simpleName
+    }
+    private lateinit var imgView: ImageView
+    private lateinit var tvText: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        imgView = findViewById(R.id.imgView)
+        tvText = findViewById(R.id.tvText)
+        imgView.setOnClickListener {
+            openImagePicker()
+        }
+
+
     }
+
+    private fun openImagePicker() {
+        ImagePicker.with(this)
+            .crop()	    			//Crop image(Optional), Check Customization for more option
+            .compress(1024)			//Final image size will be less than 1 MB(Optional)
+            .maxResultSize(1080, 1080)	//Final image resolution will be less than 1080 x 1080(Optional)
+            .start()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            //Image Uri will not be null for RESULT_OK
+            val fileUri = data?.data
+            Log.d(TAG, "onActivityResult:  fileUri:" +fileUri)
+            imgView.setImageURI(fileUri)
+
+
+        } else if (resultCode == ImagePicker.RESULT_ERROR) {
+            Toast.makeText(this, ImagePicker.getError(data), Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Task Cancelled", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
 }
