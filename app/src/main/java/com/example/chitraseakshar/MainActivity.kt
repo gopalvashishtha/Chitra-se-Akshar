3674d897c0d9481fd40360862c7f4fcae37f2aec
gopalvashishtha <gopalvashishtha2000@gmail.com>
2020-09-20 22:09:03 +0530

    Added progressbar and written other codes. ..Completed..

--- app/src/main/java/com/example/chitraseakshar/MainActivity.kt
+++ app/src/main/java/com/example/chitraseakshar/MainActivity.kt
@@ -3,23 +3,29 @@ package com.example.chitraseakshar
 import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Message
 import android.provider.MediaStore
+import android.text.TextUtils
 import android.util.Log
 import android.view.View
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
 import com.github.dhaval2404.imagepicker.ImagePicker
 import com.google.android.gms.tasks.OnFailureListener
 import com.google.android.gms.tasks.OnSuccessListener
-import com.google.firebase.ml.vision.FirebaseVision
-import com.google.firebase.ml.vision.common.FirebaseVisionImage
-import com.google.firebase.ml.vision.text.FirebaseVisionText
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.TextRecognition
+//import com.google.firebase.ml.vision.FirebaseVision
+//import com.google.firebase.ml.vision.common.FirebaseVisionImage
+//import com.google.firebase.ml.vision.text.FirebaseVisionText
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     companion object{
@@ -27,12 +33,15 @@ class MainActivity : AppCompatActivity() {
     }
     private lateinit var imgView: ImageView
     private lateinit var tvText: TextView
+    private lateinit var image: InputImage
+    private lateinit var progressBar: ProgressBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         imgView = findViewById(R.id.imgView)
         tvText = findViewById(R.id.tvText)
+        progressBar = findViewById(R.id.progressBar)
         imgView.setOnClickListener {
             openImagePicker()
         }
@@ -56,12 +65,70 @@ class MainActivity : AppCompatActivity() {
             Log.d(TAG, "onActivityResult:  fileUri:" +fileUri)
             imgView.setImageURI(fileUri)
 
+            if(fileUri != null){  //Image processing
+                processImage(fileUri)
+            }
+
 
         } else if (resultCode == ImagePicker.RESULT_ERROR) {
             Toast.makeText(this, ImagePicker.getError(data), Toast.LENGTH_SHORT).show()
         } else {
             Toast.makeText(this, "Task Cancelled", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    private fun processImage(fileUri: Uri) {
+        tvText.text = ""
+        progressBar.visibility = View.VISIBLE
+        try {
+            image = InputImage.fromFilePath(this, fileUri)
+            val recognizer = TextRecognition.getClient()
+
+            val result = recognizer.process(image)
+                .addOnSuccessListener { visionText ->
+                    // Task completed successfully
+                   Log.d(TAG, "processingImage: success")
+
+                    val resultText = visionText.text // this resultText now contains our extracted text from image
+                    Log.d(TAG, "processingImage: extractedText:" +resultText)
+                    if(TextUtils.isEmpty(resultText)){
+                        Toast.makeText(this, "No text found in the image!", Toast.LENGTH_SHORT).show()
+                        progressBar.visibility = View.GONE
+                    }
+                    else{
+                        progressBar.visibility = View.GONE
+                        tvText.text = resultText
+                    }
+
+
+//                    for (block in visionText.textBlocks) { // these loops are for individual paragraphs, words or lines
+//                        val blockText = block.text
+//                        val blockCornerPoints = block.cornerPoints
+//                        val blockFrame = block.boundingBox
+//                        for (line in block.lines) {
+//                            val lineText = line.text
+//                            val lineCornerPoints = line.cornerPoints
+//                            val lineFrame = line.boundingBox
+//                            for (element in line.elements) {
+//                                val elementText = element.text
+//                                val elementCornerPoints = element.cornerPoints
+//                                val elementFrame = element.boundingBox
+//                            }
+//                        }
+//                    }
+
+
+
+
+                }
+                .addOnFailureListener { e ->
+                    // Task failed with an exception
+                    Log.e(TAG, "processingImage: failure:"+e.message)
+                }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
     }
 
 
